f75701762caabf12de55cc07dde5a0062fbaee9c
mahis1067 <mahi.shah1067@gmail.com>
2026-09-18 15:57:20 -0400

    Exercise 6 done

--- exercises/ex06-overloading/src/main/java/MyHashing.java
+++ exercises/ex06-overloading/src/main/java/MyHashing.java
@@ -19,7 +19,7 @@ public class MyHashing {
 
   /** Creates a MyHashing whose seed starts at 0. */
   public MyHashing() {
-    // TODO: this constructor takes no arguments; leave the seed at its default.
+    this.seed = 0;
   }
 
   /**
@@ -28,7 +28,7 @@ public class MyHashing {
    * @param seed the initial seed value
    */
   public MyHashing(int seed) {
-    // TODO: store the parameter in this object's seed field.
+    this.seed = seed;
   }
 
   /**
@@ -38,8 +38,9 @@ public class MyHashing {
    * @return the seed value from before this call
    */
   public int hash(int value) {
-    // TODO
-    return 0;
+    int temp = this.seed;
+    this.seed = value;
+    return temp;
   }
 
   /**
@@ -51,8 +52,9 @@ public class MyHashing {
    * @return (previous seed + value) % MODULO
    */
   public int hash(char value) {
-    // TODO
-    return 0;
+    int temp = this.seed;
+    this.seed = value;
+    return (temp+value) % MODULO;
   }
 
   /**
@@ -65,6 +67,13 @@ public class MyHashing {
    */
   public static int hash(String value) {
     // TODO: String.toCharArray() may help.
-    return 0;
+
+    int sum = 0;
+    char[] new_values = value.toCharArray();
+
+    for (char p : new_values) {
+      sum = sum + p;    //YOU CAN SUM AN INT AND CHAR, THE CHAR AUTOMATICALLY CHANGES TO ITS INT
+    }
+    return sum;
   }
 }
